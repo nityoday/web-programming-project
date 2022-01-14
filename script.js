c262904aac072e7f5f79ad1b401d38a23173ff4d
Nityoday Tekchandani <32616539+nityoday@users.noreply.github.com>
2022-01-14 18:45:53 +0530

    Added Total Card

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 
    var result=0;
+
    function calculate(value) {
        result = result + value;
        document.getElementById("result").innerHTML = result;
+       document.getElementById("inr").style.display = "inline";
    }
